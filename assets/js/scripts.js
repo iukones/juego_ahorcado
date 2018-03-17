42b635bb10dc5654097241a0f871544547868832
Israel Martinez <iukones1@gmail.com>
2018-03-16 18:47:57 -0600

    se da estilo al teclado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -42,6 +42,25 @@ function teclado() {
         letras.appendChild(listaLetras);
         }
     }
-    teclado();
+    teclado();    
 }
 ahorcado();
+
+// function removeTransition(e) {
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+// }
+
+// function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+
+//     key.classList.add('push');
+//     audio.currentTime = 0;
+//     audio.play();
+// }
+
+//   const keys = Array.from(document.querySelectorAll('.key'));
+//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//   window.addEventListener('keydown', playSound);
